2fd400a07200e4e7ef443568861200da61a9e103
MOUKOURI Victor Eric <victorjoluke190701@gmail.com>
2025-08-17 21:45:31 +0100

    Modified cy.conf.js

--- adwa/cypress.config.js
+++ adwa/cypress.config.js
@@ -2,15 +2,9 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://127.0.0.1:5500/adwa/index.html',
+    baseUrl: 'http://127.0.0.1:3000/adwa/index.html',
     viewportWidth: 1280,
     viewportHeight: 720
-  }
-});
-
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: "http://localhost:3000", // Ton serveur Express
     supportFile: false,               // Pas de fichier support inutile
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}", // Dossier des tests
     video: true,                      // Enregistre les vidéos des tests (utile en CI)
@@ -21,20 +15,4 @@ module.exports = defineConfig({
       return config;
     },
   },
-
-  reporter: "spec",                   // Reporter par défaut
-  reporterOptions: {
-    mochaFile: "cypress/results/test-results.xml",
-    toConsole: true,
-  },
-
-  retries: {
-    runMode: 2,   // Réessaie 2 fois en mode "cypress run"
-    openMode: 0,  // Pas de retries en mode "cypress open"
-  },
-
-  viewportWidth: 1200,                // Taille de la fenêtre par défaut
-  viewportHeight: 800,
-
-  chromeWebSecurity: false,           // Pour éviter certains blocages CORS
-});
+})
--- adwa/cypress/e2e/home.cy.js
+++ adwa/cypress/e2e/home.cy.js
@@ -1,6 +1,6 @@
 describe('Test de la page d’accueil', () => {
   it('devrait afficher le contenu de la page', () => {
-    cy.visit('http://127.0.0.1:5500/adwa/index.html'); // utilise automatiquement baseUrl
+    cy.visit('http://127.0.0.1:3000/adwa/index.html'); // utilise automatiquement baseUrl
   });
 });
 
